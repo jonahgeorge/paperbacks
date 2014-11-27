a083d553f7ebeacce2bb105a38694669a2591b7d
Jonah George <jonah.george@me.com>
2014-11-27 07:37:39 -0800

    Listings tests

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  context "a listing" do
+    
+    should belong_to(:user)
+    should belong_to(:book)
+    should validate_presence_of(:user)
+    should validate_presence_of(:book)
+    should validate_presence_of(:price)
+    
+  end
+
 end
