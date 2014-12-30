92eeed64319d2b7fc7e786f5d74ea6951ec3be50
jonahgeorge <jonah.george@me.com>
2014-12-30 12:09:38 -0800

    Added failing test and fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,21 +1,21 @@
 class Book < ActiveRecord::Base
-  
+
   validates_presence_of :title
   validates_presence_of :isbn
   validates_uniqueness_of :isbn
-  
+
   has_many :listings
-  
+
   scope :with_title, ->(title) {
-    where('title LIKE ?', "%#{name}%")
+    where('title LIKE ?', "%#{title}%")
   }
-  
+
   def active_listings
     self.listings.where(is_active: true)
   end
- 
+
   def inactive_listings
     self.listings.where(is_active: false)
   end
-  
+
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -3,24 +3,36 @@ require 'test_helper'
 class BookTest < ActiveSupport::TestCase
 
   context "a book" do
-    
+
     should have_many(:listings)
     should validate_uniqueness_of(:isbn)
     should validate_presence_of(:title)
     should validate_presence_of(:isbn)
-    
+
     should "return similarly titled books for with_title" do
       the_book_thief = Book.create(title: "The Book Thief", isbn: "9780375842207")
       steal_this_book = Book.create(title: "Steal This Book", isbn: "9780899667775")
       fahrenheit_451 = Book.create(title: "Fahrenheit 451", isbn: "9781451673319")
- 
+
       books = Book.with_title("Book")
-      
+
       assert_not books.include?(fahrenheit_451), "fahrenheit_451 was in the results"
       assert books.include?(steal_this_book), "steal_this_book was not in the results"
       assert books.include?(the_book_thief), "the_book_thief was not in the results"
     end
-    
+
+    should "return no books for with_title" do
+      the_book_thief = Book.create(title: "The Book Thief", isbn: "9780375842207")
+      steal_this_book = Book.create(title: "Steal This Book", isbn: "9780899667775")
+      fahrenheit_451 = Book.create(title: "Fahrenheit 451", isbn: "9781451673319")
+
+      books = Book.with_title("Dog")
+
+      assert_not books.include?(fahrenheit_451), "fahrenheit_451 was in the results"
+      assert_not books.include?(steal_this_book), "steal_this_book was not in the results"
+      assert_not books.include?(the_book_thief), "the_book_thief was not in the results"
+    end
+
     should "return active listings for active_listing" do
       kathrine = User.create(name: "Kathrine", email: "kathrine@paperbacks.trade", password: "test")
       jonah = User.create(name: "Jonah", email: "jonah@paperbacks.trade", password: "test")
@@ -38,7 +50,7 @@ class BookTest < ActiveSupport::TestCase
       assert listings.include?(listing1), "An active listings was not included in active_listings"
       assert listings.include?(listing2), "An active listings was not included in active_listings"
     end
-    
+
     should "return inactive listings for inactive_listing" do
       kathrine = User.create(name: "Kathrine", email: "kathrine@paperbacks.trade", password: "test")
       jonah = User.create(name: "Jonah", email: "jonah@paperbacks.trade", password: "test")
@@ -56,7 +68,7 @@ class BookTest < ActiveSupport::TestCase
       assert listings.include?(listing3), "An inactive listings was not included in inactive_listings"
       assert listings.include?(listing2), "An inactive listings was not included in inactive_listings"
     end
-    
+
   end
 
 end
